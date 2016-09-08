8fa97e693d8afd6a826236e3ee369563420695b4
Marco De Paoli <depaolim@gmail.com>
2016-09-08 11:04:38 +0200

    run on linux

--- test.py
+++ test.py
@@ -4,30 +4,40 @@ from __future__ import unicode_literals
 
 import os
 import subprocess
+import sys
+import time
 import unittest
 
 import wrun
 
-DIR_NAME = os.path.dirname(os.path.realpath(__file__))
+if sys.platform == 'win32':
+    EXECUTABLE_NAME = "sample.bat"
+else:
+    EXECUTABLE_NAME = "sample.sh"
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+EXECUTABLE_PATH = os.path.join("test_executables", EXECUTABLE_NAME)
 
 
 class AcceptanceTest(unittest.TestCase):
         def setUp(self):
-            self.daemon = subprocess.Popen("python wrun/__init__.py")
+            self.daemon = subprocess.Popen(["python", "wrun/__init__.py", CWD])
+            time.sleep(0.5)
             
         def tearDown(self):
             self.daemon.terminate()
             
         def test_execute_with_param_P1(self):
             client = wrun.Client("localhost")
-            result = client.run("sample.bat", "P1")
-            expected = os.linesep.join([DIR_NAME, "hello P1", ""])
+            result = client.run(EXECUTABLE_PATH, "P1")
+            expected = os.linesep.join([CWD, "hello P1", ""])
             self.assertEqual(result, expected)
 
         def test_execute_with_param_P2(self):
             client = wrun.Client("localhost")
-            result = client.run("sample.bat", "P2")
-            self.assertEqual(result, "{}\r\nhello P2\r\n".format(DIR_NAME))
+            result = client.run(EXECUTABLE_PATH, "P2")
+            expected = os.linesep.join([CWD, "hello P2", ""])
+            self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
--- wrun/__init__.py
+++ wrun/__init__.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os.path
 import subprocess
+import sys
 
 import Pyro4
 
@@ -19,13 +21,16 @@ class Client:
 class Executor:
     @Pyro4.expose
     def run(self, exe_name, *args):
-        result = subprocess.check_output([exe_name] + list(args))
+        cmd = [os.path.join(self.EXE_PATH, exe_name)]
+        cmd.extend(args)
+        result = subprocess.check_output(cmd)
         return result
         
         
 class Server:
-    def __init__(self):
+    def __init__(self, exe_path):
         self.daemon = Pyro4.Daemon(host="localhost", port=3333)
+        Executor.EXE_PATH = exe_path
         self.uri = self.daemon.register(Executor, "Executor")
         
     def start(self):
@@ -36,5 +41,7 @@ class Server:
         
         
 if __name__ == '__main__':
-    s = Server()
+    # just for test purpose
+    exe_path = sys.argv[1]
+    s = Server(exe_path)
     s.start()
