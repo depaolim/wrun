4533309cd50c05dd32a74e41c038739567e1cf30
Marco De Paoli <depaolim@gmail.com>
2016-09-08 12:16:06 +0200

    pep8

--- test.py
+++ test.py
@@ -23,23 +23,23 @@ PORT = "3333"
 
 
 class AcceptanceTest(unittest.TestCase):
-        def setUp(self):
-            self.daemon = subprocess.Popen(["python", "wrun/__init__.py", CWD, PORT])
-            time.sleep(0.1)
-            self.client = wrun.Client(HOST_NAME, PORT)
+    def setUp(self):
+        self.daemon = subprocess.Popen(["python", "wrun/__init__.py", CWD, PORT])
+        time.sleep(0.1)
+        self.client = wrun.Client(HOST_NAME, PORT)
 
-        def tearDown(self):
-            self.daemon.terminate()
+    def tearDown(self):
+        self.daemon.terminate()
 
-        def test_execute_with_param_P1(self):
-            result = self.client.run(EXECUTABLE_PATH, "P1")
-            expected = os.linesep.join([CWD, "hello P1", ""])
-            self.assertEqual(result, expected)
+    def test_execute_with_param_P1(self):
+        result = self.client.run(EXECUTABLE_PATH, "P1")
+        expected = os.linesep.join([CWD, "hello P1", ""])
+        self.assertEqual(result, expected)
 
-        def test_execute_with_param_P2(self):
-            result = self.client.run(EXECUTABLE_PATH, "P2")
-            expected = os.linesep.join([CWD, "hello P2", ""])
-            self.assertEqual(result, expected)
+    def test_execute_with_param_P2(self):
+        result = self.client.run(EXECUTABLE_PATH, "P2")
+        expected = os.linesep.join([CWD, "hello P2", ""])
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
