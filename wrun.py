65326a280fd4b58a24039401fe1824f7784219c5
Marco De Paoli <depaolim@gmail.com>
2016-10-12 15:52:12 +0200

    base logging

--- wrun.py
+++ wrun.py
@@ -13,12 +13,13 @@ import Pyro4
 
 CommunicationError = Pyro4.errors.CommunicationError
 logging.basicConfig(filename='wrun.log', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class Executor:
     @Pyro4.expose
     def run(self, exe_name, *args):
-        logging.debug("Executor.run %s %s", exe_name, " ".join(args))
+        logger.debug("Executor.run %s %s", exe_name, " ".join(args))
         cmd = [os.path.join(self.EXE_PATH, exe_name)]
         cmd.extend(args)
         result = subprocess.check_output(cmd, cwd=self.EXE_PATH)
@@ -29,20 +30,26 @@ class Server:
     EXECUTOR_CLASS = Executor
 
     def __init__(self, exe_path, port, hmackey="", host=""):
+        logger.debug("Server setup...")
         if not host:
             host = socket.gethostname()
+        logger.debug("setting host: %s, port: %s", host, port)
         self.daemon = Pyro4.Daemon(host=host, port=int(port))
         if hmackey:
+            logger.debug("setting Hmac Key")
             self.daemon._pyroHmacKey = bytes(hmackey)
         executor_class = self.EXECUTOR_CLASS
         executor_class.EXE_PATH = exe_path
+        logger.debug("registering executor")
         self.uri = self.daemon.register(executor_class, executor_class.__name__)
 
     def start(self):
+        logger.debug("Server starting...")
         self.daemon.requestLoop()
 
     def stop(self):
         self.daemon.shutdown()
+        logger.debug("Server stopped")
 
 
 class Client:
