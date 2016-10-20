b2e68f8b0c031390499926a35d83e635d0cb924a
Marco De Paoli <depaolim@gmail.com>
2016-10-20 11:28:42 +0200

    refactoring

--- wrun.py
+++ wrun.py
@@ -20,14 +20,14 @@ SERVER = None
 
 
 CommunicationError = Pyro4.errors.CommunicationError
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 logging.basicConfig(filename='wrun.log', level=logging.DEBUG)
 
 
 class Executor:
     @Pyro4.expose
     def run(self, exe_name, *args):
-        logger.debug("Executor.run %s %s", exe_name, " ".join(args))
+        log.debug("Executor.run %s %s", exe_name, " ".join(args))
         cmd = [os.path.join(self.EXE_PATH, exe_name)]
         cmd.extend(args)
         result = subprocess.check_output(cmd, cwd=self.EXE_PATH)
@@ -38,25 +38,25 @@ class Server:
     EXECUTOR_CLASS = Executor
 
     def __init__(self, exe_path, port, hmackey="", host=""):
-        logger.debug("Server setup...")
+        log.debug("Server setup...")
         if not host:
             host = socket.gethostname()
-        logger.debug("setting host: %s, port: %s", host, port)
+        log.debug("setting host: %s, port: %s", host, port)
         self.daemon = Pyro4.Daemon(host=host, port=int(port))
         if hmackey:
-            logger.debug("setting Hmac Key")
+            log.debug("setting Hmac Key")
             self.daemon._pyroHmacKey = bytes(hmackey)
         executor_class = self.EXECUTOR_CLASS
         executor_class.EXE_PATH = exe_path
-        logger.debug("registering executor")
+        log.debug("registering executor")
         self.uri = self.daemon.register(executor_class, executor_class.__name__)
 
     def start(self):
-        logger.debug("Server starting...")
+        log.debug("Server starting...")
         self.daemon.requestLoop()
 
     def stop(self):
-        logger.debug("Server stopping...")
+        log.debug("Server stopping...")
 
         # !!! TODO !!!
         if sys.platform == 'win32':
@@ -64,7 +64,7 @@ class Server:
         else:
             self.daemon.close()
 
-        logger.debug("Server stopped")
+        log.debug("Server stopped")
 
 
 class Client:
