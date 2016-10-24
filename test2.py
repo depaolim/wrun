c61b00d9154da5758dc4cd0491453f0d4d0e3c0e
Marco De Paoli <depaolim@gmail.com>
2016-10-24 09:59:44 +0200

    new version based on sockets (no more Pyro4)

---
+++ test2.py
@@ -0,0 +1,184 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import json
+import logging
+import multiprocessing
+import os
+import signal
+import sys
+import time
+import unittest
+
+from wrun2 import client, daemon, executor
+
+if sys.platform == 'win32':
+    EXECUTABLE_NAME = "sample.bat"
+else:
+    EXECUTABLE_NAME = "sample.sh"
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+EXECUTABLE_PATH = os.path.join(CWD, "test_executables")
+
+
+class TLogTestMixin(object):
+    @staticmethod
+    def _get_log(path):
+        with open(path) as f:
+            return f.read()
+
+    @staticmethod
+    def _log_path(func):
+        return "transport_" + func.func_name + ".log"
+
+    @staticmethod
+    def init_log_file(path):
+        logging.basicConfig(filename=path, level=logging.DEBUG, filemode='a')
+        logging.FileHandler(path, mode='w').close()  # log cleanup
+
+    @classmethod
+    def logged_func(cls, func, *args):
+        cls.init_log_file(cls._log_path(func))
+        return func(*args)
+
+    def assertLogContains(self, func, expected):
+        self.assertIn(expected, self._get_log(self._log_path(func)))
+
+
+class ProcessFunc(object):
+    def _kill(self):
+        if sys.platform == 'win32':
+            self._process.terminate()
+        else:
+            os.kill(self._process.pid, signal.SIGINT)
+
+    def __init__(self, func):
+        self._queue = multiprocessing.Queue()
+        self._process = multiprocessing.Process(target=lambda: self._queue.put(func()))
+        self._process.start()
+        time.sleep(0.5)
+
+    def join(self):
+        self._process.join()
+
+    def stop(self, ignore_errors=False):
+        if not ignore_errors or self._process.is_alive():
+            self._kill()
+        self.join()
+
+    @property
+    def result(self):
+        return self._queue.get()
+
+
+class TestClientServer(TLogTestMixin, unittest.TestCase):
+    SERVER_ADDRESS = ('localhost', 3333)
+
+    def _run_process_func(self, func, *args):
+        return ProcessFunc(lambda: self.logged_func(func, *args))
+
+    def setUp(self):
+        self.s = self._run_process_func(daemon, self.SERVER_ADDRESS,)
+
+    def tearDown(self):
+        self.s.stop(ignore_errors=True)
+
+    def test_server_is_listening(self):
+        self.assertLogContains(daemon, "waiting for a connection...")
+
+    @unittest.skipIf(sys.platform == 'win32', "no clean shutdown on Windows")
+    def test_server_shutdown(self):
+        self.s.stop()
+        self.assertLogContains(daemon, "closing server socket...")
+        self.assertLogContains(daemon, "closed server socket")
+
+    def test_client_connect(self):
+        c = self._run_process_func(client, self.SERVER_ADDRESS, "prova")
+        c.join()
+        self.assertEqual(c.result, "prova")
+        self.assertLogContains(client, "CLIENT: connecting '('localhost', 3333)' ...")
+        self.assertLogContains(client, "CLIENT: connected")
+        self.assertLogContains(client, "CLIENT: sending 'prova'")
+        self.assertLogContains(client, "CLIENT: sent")
+        self.assertLogContains(client, "CLIENT: receiving...")
+        self.assertLogContains(daemon, "SERVER: connection from ('127.0.0.1', ")
+        self.assertLogContains(daemon, "SERVER: receiving...")
+        self.assertLogContains(daemon, "SERVER: received 'prova'")
+        self.assertLogContains(daemon, "SERVER: received ''")
+        self.assertLogContains(daemon, "SERVER: no more data to receive")
+        self.assertLogContains(daemon, "SERVER: sending 'prova'")
+        self.assertLogContains(daemon, "SERVER: sent")
+        self.assertLogContains(daemon, "SERVER: closing client socket")
+        self.assertLogContains(daemon, "SERVER: closed client socket")
+        self.assertLogContains(client, "CLIENT: received 'prova'")
+        self.assertLogContains(client, "CLIENT: received ''")
+        self.assertLogContains(client, "CLIENT: no more data to receive")
+        self.assertLogContains(client, "CLIENT: closing")
+        self.assertLogContains(client, "CLIENT: closed")
+
+
+class TestExecutor(unittest.TestCase):
+    def test_run_P1(self):
+        command = [EXECUTABLE_NAME, ["P1"]]
+        result = executor(EXECUTABLE_PATH, json.dumps(command))
+        expected = os.linesep.join([EXECUTABLE_PATH, "hello P1", ""])
+        self.assertEqual(result, expected)
+
+    def test_run_P2(self):
+        command = [EXECUTABLE_NAME, ["P2"]]
+        result = executor(EXECUTABLE_PATH, json.dumps(command))
+        expected = os.linesep.join([EXECUTABLE_PATH, "hello P2", ""])
+        self.assertEqual(result, expected)
+
+
+class TestAcceptance(TLogTestMixin, unittest.TestCase):
+    SERVER_ADDRESS = ('localhost', 3333)
+
+    def _run_process_func(self, func, *args):
+        return ProcessFunc(lambda: self.logged_func(func, *args))
+
+    def setUp(self):
+        self.s = self._run_process_func(
+            daemon, self.SERVER_ADDRESS,
+            lambda exe_name, *args: executor(EXECUTABLE_PATH, exe_name, *args))
+
+    def tearDown(self):
+        self.s.stop(ignore_errors=True)
+
+    def test_server_is_listening(self):
+        self.assertLogContains(daemon, "waiting for a connection...")
+
+    @unittest.skipIf(sys.platform == 'win32', "no clean shutdown on Windows")
+    def test_server_shutdown(self):
+        self.s.stop()
+        self.assertLogContains(daemon, "closing server socket...")
+        self.assertLogContains(daemon, "closed server socket")
+
+    def test_client_connect(self):
+        c = self._run_process_func(client, self.SERVER_ADDRESS, json.dumps([EXECUTABLE_NAME, ["P1"]]))
+        c.join()
+        expected = os.linesep.join([EXECUTABLE_PATH, "hello P1", ""])
+        self.assertEqual(c.result, expected)
+        self.assertLogContains(client, "CLIENT: connecting '('localhost', 3333)' ...")
+        self.assertLogContains(client, "CLIENT: connected")
+        self.assertLogContains(client, "CLIENT: sending '[\"sample.sh\", [\"P1\"]]'")
+        self.assertLogContains(client, "CLIENT: sent")
+        self.assertLogContains(client, "CLIENT: receiving...")
+        self.assertLogContains(daemon, "SERVER: connection from ('127.0.0.1', ")
+        self.assertLogContains(daemon, "SERVER: receiving...")
+        self.assertLogContains(daemon, "SERVER: received '[\"sample.sh\", [\"P1\"]]'")
+        self.assertLogContains(daemon, "SERVER: received ''")
+        self.assertLogContains(daemon, "SERVER: no more data to receive")
+        self.assertLogContains(daemon, "SERVER: sending '{}'".format(expected))
+        self.assertLogContains(daemon, "SERVER: sent")
+        self.assertLogContains(daemon, "SERVER: closing client socket")
+        self.assertLogContains(daemon, "SERVER: closed client socket")
+        self.assertLogContains(client, "CLIENT: received '{}'".format(expected))
+        self.assertLogContains(client, "CLIENT: received ''")
+        self.assertLogContains(client, "CLIENT: no more data to receive")
+        self.assertLogContains(client, "CLIENT: closing")
+        self.assertLogContains(client, "CLIENT: closed")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ wrun2.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import json
+import logging
+import os
+import socket
+import subprocess
+
+BUFFER_SIZE = 255
+
+log = logging.getLogger(__name__)
+
+
+class Socket(socket.socket):
+    def __init__(self):
+        super(Socket, self).__init__(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def daemon(server_address, execute=lambda request: request, condition=lambda: True):
+    ss = Socket()
+    ss.bind(server_address)
+    ss.listen(1)
+    try:
+        while condition():
+            log.info("SERVER: waiting for a connection...")
+            sc, ad = ss.accept()
+            log.info("SERVER: connection from %s", ad)
+            request = ""
+            while True:
+                log.info("SERVER: receiving...")
+                data = sc.recv(BUFFER_SIZE)
+                log.info("SERVER: received '%s'", data)
+                if not data:
+                    log.info("SERVER: no more data to receive")
+                    break
+                request += data
+            response = execute(request)
+            log.info("SERVER: sending '%s' ...", response)
+            sc.sendall(response)
+            log.info("SERVER: sent")
+            log.info("SERVER: closing client socket...")
+            sc.close()
+            log.info("SERVER: closed client socket")
+    finally:
+        log.info("SERVER: closing server socket...")
+        ss.close()
+        log.info("SERVER: closed server socket")
+
+
+def client(server_address, request):
+    ss = Socket()
+    log.info("CLIENT: connecting '%s' ...", server_address)
+    ss.connect(server_address)
+    log.info("CLIENT: connected")
+    log.info("CLIENT: sending '%s' ...", request)
+    ss.sendall(request)
+    ss.shutdown(socket.SHUT_WR)
+    log.info("CLIENT: sent")
+    response = ""
+    while True:
+        log.info("CLIENT: receiving...")
+        data = ss.recv(BUFFER_SIZE)
+        log.info("CLIENT: received '%s'", data)
+        if not data:
+            log.info("CLIENT: no more data to receive")
+            break
+        response += data
+    log.info("CLIENT: closing...")
+    ss.close()
+    log.info("CLIENT: closed")
+    return response
+
+
+def executor(exe_path, command):
+    exe_name, args = json.loads(command)
+    log.debug("executor %s %s", exe_name, " ".join(args))
+    cmd = [os.path.join(exe_path, exe_name)]
+    cmd.extend(args)
+    result = subprocess.check_output(cmd, cwd=exe_path)
+    return result
